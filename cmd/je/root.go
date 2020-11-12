4ca02d7d59709049855cb62cf0b93893bf2a82a5
wooos <819844849@qq.com>
2020-11-12 19:43:47 +0800

    Add update,completion,version command

---
+++ cmd/je/root.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+
+	"github.com/spf13/cobra"
+)
+
+func newRootCmd(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:          "je",
+		Short:        "Stream editor for json",
+		Long:         "",
+		SilenceUsage: true,
+	}
+
+	cmd.AddCommand(
+		newCompletionCommand(),
+		newUpdateCmd(),
+		newVersionCmd(out),
+	)
+	return cmd
+}
